b9b58611abc7689290582686d550374a550a9fdb
bdfc1988 <r.ettenfield@googlemail.com>
2022-09-01 16:35:32 +0800

    add extra routes

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {res.redirect('/index.html')});
+app.get('/quadratics', (req, res) => {res.redirect('/index.html')});
+app.get('/circles', (req, res) => {res.redirect('/index.html')});
+app.get('/coordinategeometry', (req, res) => {res.redirect('/index.html')});
+app.get('/series', (req, res) => {res.redirect('/index.html')});
+app.get('/binomial', (req, res) => {res.redirect('/index.html')});
+app.get('/functions', (req, res) => {res.redirect('/index.html')});
+app.get('/differentiation', (req, res) => {res.redirect('/index.html')});
+app.get('/integration', (req, res) => {res.redirect('/index.html')});
+app.get('/circularmeasure', (req, res) => {res.redirect('/index.html')});
+app.get('/trigonometry', (req, res) => {res.redirect('/index.html')});
+app.get('/register', (req, res) => {res.redirect('/index.html')});
+app.get('/questions', (req, res) => {res.redirect('/index.html')});
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
