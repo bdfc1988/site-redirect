5208fae229194dd50826c27393eea9db7c7fb2d4
bdfc1988 <r.ettenfield@googlemail.com>
2022-09-01 16:27:24 +0800

    ready to deploy

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {res.redirect('/index.html')});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+    PORT,
+    console.log(
+      `Server is running on port ${PORT}`
+    )
+  );
